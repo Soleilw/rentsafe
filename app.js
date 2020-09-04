436efd65c6427d00f1aaea433e948a9f0c90d462
Ammoxy <15976506547@qq.com>
2020-09-04 14:47:07 +0800

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ App({
       })
     }
     // 开关配置
-    var version = '1.0.12';
+    var version = '1.0.13';
     global.configs(version).then(res => {
       wx.setStorageSync('openFace', res.config_value);
     })
