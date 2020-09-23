c461a3883bf36d98b82bc9ee2ad2f21ed700542b
Ammoxy <15976506547@qq.com>
2020-09-23 10:45:34 +0800

    update

--- api/baseUrl.js
+++ api/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl = {
     'host': 'https://chu.fengniaotuangou.cn/api',
-    // 'host': 'http://192.168.0.108/api',
+    // 'host': 'http://192.168.0.104/api',
 }
 
 module.exports = baseUrl;
